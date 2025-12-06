dc54506ea791614fb2af8facdaaa8d45d03c8b0c
Menaka Jayawardena <menaka12350@gmail.com>
2025-12-06 15:56:05 +0530

    Fix conole build failure

---
+++ console/apps/webapp/public/config.js
@@ -0,0 +1,13 @@
+window.__RUNTIME_CONFIG__ = {
+    authConfig: {
+      signInRedirectURL: 'http://localhost:3000',
+      signOutRedirectURL: 'http://localhost:3000',
+      clientID: '',
+      baseUrl: 'http://localhost:8080',
+      scope: ['openid', 'profile'],
+      storage: 'sessionStorage'
+    },
+    disableAuth: true,
+    apiBaseUrl: 'http://localhost:8080',
+    obsApiBaseUrl: 'http://localhost:9098'
+  };
