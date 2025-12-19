aa3227827eb1d34e2d51abb2792797fdf29b1e88
Menaka Jayawardena <menaka12350@gmail.com>
2025-12-19 15:59:14 +0530

    update

---
+++ console/apps/webapp/public/config.js
@@ -0,0 +1,14 @@
+
+window.__RUNTIME_CONFIG__ = {
+    authConfig: {
+      signInRedirectURL: 'http://localhost:3000/signin',
+      signOutRedirectURL: 'http://localhost:3000/signout',
+      clientID: 'null',
+      baseUrl: 'http://localhost:8080',
+      scope: ['openid', 'profile'],
+      storage: 'sessionStorage'
+    },
+    disableAuth: true,
+    apiBaseUrl: 'http://localhost:8080',
+    obsApiBaseUrl: 'http://localhost:9098'
+};
